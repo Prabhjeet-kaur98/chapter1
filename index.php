bf0854127aa96b7da4e2a9e57d7ae404fd540cca
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-09-12 17:18:33 -0400

    add new section 7

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <?php 
-$offers = [
-  ['name' => 'Toffee', 'price' => 5, 'stock' => 120,],
-  ['name' => 'Mints', 'price' => 3, 'stock' => 66,],
-  ['name' => 'FUDGE', 'price' => 4, 'stock' => 97,],
-  ['name' => 'Choclate', 'price' => 2, 'stock' => 83,],
-  
-   ];
+$name      = 'Ivy';
+$favorites = ['Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
   <head>
+    <title>Echo Shorthand</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>offers</h2>
-    <p><?php echo $offers[0]['name']; ?> -
-    $<p><?php echo $offers[0]['price']; ?> </p>
-    <p><?php echo $offers[1]['name']; ?> -
-    $<p><?php echo $offers[1]['price']; ?> </p>
-    <?php echo $offers[2]['name']; ?> -
-    $<p><?php echo $offers[2]['price']; ?> </p>
-    <?php echo $offers[3]['name']; ?> -
-    $<p><?php echo $offers[3]['price']; ?> </p>
-</body>
+    <h2>Welcome <?= $name ?></h2>
+    <p>Your favorite type of candy is:
+       <?= $favorites[0] ?>.</p>
+  </body>
 </html>
