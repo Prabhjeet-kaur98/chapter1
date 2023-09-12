30433771fe5371ccde8b81ca1b06cb26979badd6
Prabhjeet-kaur98 <122941904+Prabhjeet-kaur98@users.noreply.github.com>
2023-09-12 17:42:46 -0400

    section 11 add

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php 
 $item    = 'Chocolate';
-$stock   = 8;
-$wanted  = 5;
-$can_buy = ($wanted <= $stock);
+$stock   = 5;
+$wanted  = 3;
+$deliver = true;
+$can_buy = (($wanted <= $stock) && ($deliver == true));
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Comparison Operators</title>
+    <title>Logical Operators</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Shopping Cart</h2>
+    <h1>Shopping Cart</h1>
     <p>Item:    <?= $item ?></p>
     <p>Stock:   <?= $stock ?></p>
-    <p>Wanted:  <?= $wanted ?></p>
+    <p>Wanted: <?= $wanted ?></p>
     <p>Can buy: <?= $can_buy ?></p>
   </body>
-</html
+</html>
